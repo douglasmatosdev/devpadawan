735d83e8c02658f631b57a6eec840864faa3cce2
douglasmatosdev <douglasmatosdev@gmail.com>
2021-02-28 19:05:35 -0300

    remove width and height by svg logo for test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <title>Home page</title>
             </Head>
             <main>
-                <JediLogo width={250} height={250} />
+                <JediLogo />
                 <h1>Dev Padawan</h1>
                 <p>My studies about all</p>
             </main>
