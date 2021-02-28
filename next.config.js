4cbba36a495a676616cea274c765ef9b637299de
douglasmatosdev <douglasmatosdev@gmail.com>
2021-02-28 20:59:49 -0300

    refactor(next.config): add configs and install dependencies for read local files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
 const withImages = require('next-images')
 
-module.exports = withImages({
-  esModule: true
-})
+module.exports = module.exports = {
+  withImages: withImages({
+    esModule: true
+  }),
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/about/',
+        destination: '/sobre/',
+        permanent: true // 308 | false=307
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*/',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ]
+  }
+}
