0ff7c52a3e759bb7216ff85ab166e5d690f7a351
douglasmatosdev <douglasmatosdev@gmail.com>
2021-03-07 20:06:01 -0300

    Change local to use Global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
-import GlobalStyle from '../styles/global'
 import { getAllPosts } from '../../scripts/blog/getAllPosts'
 
 import Head from '../infra/components/Head'
 import Navbar from '../components/Navbar'
 
 export default function Home({ posts }) {
+    const [{metadata}] = posts
+    const { title, date, excerpt, slug } = metadata
+
     return (
         <div>
-            <GlobalStyle />
             <Head title="DevPadawan" />
             <Navbar />
             <header className="headerContainer">
@@ -24,14 +25,14 @@ export default function Home({ posts }) {
                 <h1>Posts</h1>
                 {posts.map((post, i) => {
                     return (
-                        <article key={`${post.metadata.title}-${i}`} className="postsContainer__post">
+                        <article key={`${title}-${i}`} className="postsContainer__post">
                             <h2>
-                                <Link href={`/post/${post.metadata.slug}`}>
-                                    {post.metadata.title}
+                                <Link href={`/post/${slug}`}>
+                                    {title}
                                 </Link>
                             </h2>
                             <p>
-                                {post.metadata.excerpt}
+                                {excerpt}
                             </p>
                         </article>
                     )
